046393bcf2d7e211bd3cbcd92e6440dbb3c23a16
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2018-03-11 15:28:38 +0300

    lab1: add comments to code

--- lab1/main.c
+++ lab1/main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Structure for complex numbers
+ *
+ * re - real part
+ * im - imaginary part
+ */
 struct ComplexNumber {
   double re;
   double im;
 };
 
+/**
+ * Function, that returns the result of the multiplication of two complex numbers
+ *
+ * (a + ib)(c + id) = ac + ibc + iad - bd = (ac - bd) + i(bc + ad)
+ *
+ * @param a first complex number
+ * @param b second complex number
+ * @return multiplication of a and b
+ */
 struct ComplexNumber multiply(struct ComplexNumber a, struct ComplexNumber b) {
   struct ComplexNumber res;
 
@@ -15,6 +30,15 @@ struct ComplexNumber multiply(struct ComplexNumber a, struct ComplexNumber b) {
   return res;
 }
 
+/**
+ * Function, that returns the result of the addition of two complex numbers
+ *
+ * (a + ib) + (c + id) = (a + c) + i(b + d)
+ *
+ * @param a first complex number
+ * @param b second complex number
+ * @return sum of a and b
+ */
 struct ComplexNumber add(struct ComplexNumber a, struct ComplexNumber b) {
   struct ComplexNumber res;
 
@@ -30,6 +54,16 @@ int main() {
 
   scanf("%d", &n);
 
+  /*
+   * array for storing polynomial coefficients
+   *
+   * P(x) = a0 + a1*x + a2*x^2 + ... + an*x^n
+   *
+   * a[0] = a0
+   * a[1] = a1
+   * ...
+   * a[n] = an
+   */
   struct ComplexNumber* a = (struct ComplexNumber*) malloc(sizeof(struct ComplexNumber) * (n + 1));
 
   for (int i = 0; i <= n; i++) {
@@ -45,6 +79,9 @@ int main() {
     scanf("%lf", &x.re);
     scanf("%lf", &x.im);
 
+    /*
+     * P(x) = a0 + a1*x + a2*x^2 + ... + an*x^n = a0 + x(a1 + x(a2 + ... + x(an-1 + an*x)))
+     */
     struct ComplexNumber res = a[n];
 
     for (int j = n - 1; j >= 0; j--) {
