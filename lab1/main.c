4247f3162c7e8457c0035f488c9cb92d397509d8
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2018-03-11 15:09:03 +0300

    lab1: implement algorithm for complex numbers

--- lab1/main.c
+++ lab1/main.c
@@ -1,32 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct ComplexNumber {
+  double re;
+  double im;
+};
+
+struct ComplexNumber multiply(struct ComplexNumber a, struct ComplexNumber b) {
+  struct ComplexNumber res;
+
+  res.re = a.re * b.re - a.im * b.im;
+  res.im = a.im * b.re + a.re * b.im;
+
+  return res;
+}
+
+struct ComplexNumber add(struct ComplexNumber a, struct ComplexNumber b) {
+  struct ComplexNumber res;
+
+  res.re = a.re + b.re;
+  res.im = a.im + b.im;
+
+  return res;
+}
+
 int main() {
   int n;
   int m;
 
   scanf("%d", &n);
 
-  int* a = (int*) malloc(sizeof(int) * (n + 1));
+  struct ComplexNumber* a = (struct ComplexNumber*) malloc(sizeof(struct ComplexNumber) * (n + 1));
 
   for (int i = 0; i <= n; i++) {
-    scanf("%d", &a[i]);
+    scanf("%lf", &a[i].re);
+    scanf("%lf", &a[i].im);
   }
 
   scanf("%d", &m);
 
   for (int i = 0; i < m; i++) {
-    int x;
+    struct ComplexNumber x;
 
-    scanf("%d", &x);
+    scanf("%lf", &x.re);
+    scanf("%lf", &x.im);
 
-    int res = a[n];
+    struct ComplexNumber res = a[n];
 
     for (int j = n - 1; j >= 0; j--) {
-      res = res * x + a[j];
+      res = add(multiply(res, x), a[j]);
     }
 
-    printf("%d\n", res);
+    printf("%lf %lf\n", res.re, res.im);
   }
 
   return 0;
