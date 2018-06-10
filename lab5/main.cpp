270783f6f253f92c7f3ee653d0c85a131e760cbe
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2018-06-10 22:23:06 +0300

    lab5: complete lab

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -10,6 +10,10 @@ double df(double x) {
   return -4 * cos(x) * exp(-sin(x))- (2 * x - 1)/(x * x - x + 1);
 }
 
+double ddf(double x) {
+  return exp(-sin(x)) * (sin(x) + cos(x)) - (2 * (x * x + 1) - pow(2 * x - 1, 2)) / pow(x * x - x + 1, 2);
+}
+
 double max(double a, double b) {
   return a > b ? a : b;
 }
@@ -19,6 +23,7 @@ int main() {
   int h = (b - a)/n;
 
   double inaccuracy_df = 0;
+  double inaccuracy_ddf = 0;
 
   std::vector<double> df_calculated;
   std::vector<double> ddf_calculated;
@@ -27,15 +32,30 @@ int main() {
     df_calculated.push_back((f(x + h) - f(x - h))/ 2 * h);
   }
 
+  for (int i = 1; i < df_calculated.size() - 1; i++) {
+    ddf_calculated.push_back((df_calculated[i + 1] - df_calculated[i - 1])/ 2 * h);
+  }
+
   int i = 0;
 
   for (int x = a; x <= b; x += h) {
     inaccuracy_df = max(inaccuracy_df, fabs(df(x) - df_calculated[i + 1]));
-    std::cout << x << " " << df_calculated[i + 1] << " " << df(x) << std::endl;
+    inaccuracy_ddf = max(inaccuracy_ddf, fabs(ddf(x) - ddf_calculated[i + 1]));
+
+    std::cout << x
+              << " "
+              << df(x)
+              << " "
+              << df_calculated[i + 1]
+              << " "
+              << ddf(x)
+              << " "
+              << ddf_calculated[i]
+              << std::endl;
     i++;
   }
 
-  std::cout << inaccuracy_df << std::endl;
+  std::cout << inaccuracy_df << " " << inaccuracy_ddf << std::endl;
 
   return 0;
 }
